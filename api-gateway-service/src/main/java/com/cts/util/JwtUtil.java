957b037a8d7b92fc527576ae4591ab2702b0025e
M <2393120@cognizant.com>
2025-05-29 15:29:09 +0530

    Final Upload

---
+++ api-gateway-service/src/main/java/com/cts/util/JwtUtil.java
@@ -0,0 +1,23 @@
+package com.cts.util;
+
+import java.nio.charset.StandardCharsets;
+
+import java.security.Key;
+
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Jwts;
+
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtUtil {
+
+	// Your preset secret value (should be loaded from configuration in production)
+	public static final String SECRET = "FAwq03T8oLdM38iVlWZqEsKNG3fjfa6SRbPGzMC1hQn/Sf7M/IKjP3TVUz9uOIYCA5z2nNJx436/tMnSAKQCow==";
+
+	public void validateToken(String token) {
+		Key key = Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+		Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+	}
+}
---
+++ enrollment-service/src/main/java/com/cts/entity/User.java
@@ -0,0 +1,36 @@
+package com.cts.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int userId;
+	
+	private String name;
+	
+	private String role;
+	
+	@Column(unique = true, nullable = false)
+	private String email;
+	
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	private String password;
+	
+}
